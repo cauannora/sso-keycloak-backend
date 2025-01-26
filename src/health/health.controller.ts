5a14bf98d3c1796e1b4b2cadb11c23d58b873156
Cauan Norberto <cauannora@gmail.com>
2025-01-26 02:41:10 -0300

    Adding more health checks

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+import { DiskHealthIndicator, HealthCheck, HealthCheckService, HttpHealthIndicator, MemoryHealthIndicator } from '@nestjs/terminus';
 import { Public } from 'nest-keycloak-connect';
 
 @Controller('health')
@@ -7,15 +7,53 @@ export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private disk: DiskHealthIndicator,
+    private memory: MemoryHealthIndicator,
   ) {}
 
-  @Get()
+  @Get('live')
+  @Public()
+  @HealthCheck()
+  checkLiveness() {
+    return this.health.check([
+      // Verifica se o Keycloak está disponível
+      () => this.http.pingCheck('keycloak', process.env.KEYCLOAK_HEALTH_URL),
+      
+      // Verifica espaço em disco (mínimo 50MB disponíveis)
+      async () =>
+        this.disk.checkStorage('disk', {
+          thresholdPercent: 0.9,
+          path: '/',
+        }),
+
+      // Verifica se a memória heap disponível é suficiente (mínimo 150MB)
+      async () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+
+      // Verifica memória do processo (mínimo 150MB livres)
+      async () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
+    ]);
+  }
+
+  @Get('ready')
   @Public()
   @HealthCheck()
   keycloakCheck() {
     return this.health.check([
-      () => this.http.pingCheck('keycloak', process.env.KEYCLOAK_HEALTH_URL)
+      // Verifica se o Keycloak está disponível
+      () => this.http.pingCheck('keycloak', process.env.KEYCLOAK_HEALTH_URL),
+
+      // Verifica espaço em disco (mínimo 50MB disponíveis)
+      async () =>
+        this.disk.checkStorage('disk', {
+          thresholdPercent: 0.9,
+          path: '/',
+        }),
+
+      // Verifica se a memória heap disponível é suficiente (mínimo 150MB)
+      async () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+
+      // Verifica memória do processo (mínimo 150MB livres)
+      async () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
     ]);
   }
-
 }
